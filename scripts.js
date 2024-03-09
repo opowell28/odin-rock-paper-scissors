ba389a04c053ba6d9ffa0f958b8e22f74e4f39d1
Owen Powell <opowell2899@gmail.com>
2024-03-09 17:32:15 -0500

    Build logic to play a single round

--- scripts.js
+++ scripts.js
@@ -16,4 +16,58 @@ function getComputerChoice() {
 // ask for user input
 function getPlayerChoice() {
     let playerChoice = prompt("Enter 'rock,' 'paper,' or 'scissors.'");
+    playerChoice = playerChoice.toLowerCase();
+    return playerChoice;
 }
+
+// play a single round
+function playOneRound(playerChoice, computerChoice) {
+    switch(playerChoice) {
+        case "rock":
+            if(computerChoice === "rock") {
+                alert("Both chose rock, it's a tie.");
+            }
+            else if(computerChoice === "paper") {
+                alert("You lose, paper beats rock.");
+            }
+            else if(computerChoice === "scissors") {
+                alert("You win, rock beats scissors.");
+            }
+            else {
+                alert("ERROR");
+            }
+            break;
+        case "paper":
+            if(computerChoice === "rock") {
+                alert("You win, paper beats rock.");
+            }
+            else if(computerChoice === "paper") {
+                alert("Both chose paper, it's a tie.");
+            }
+            else if(computerChoice === "scissors") {
+                alert("You lose, scissors beats paper.");
+            }
+            else {
+                alert("ERROR");
+            }
+            break;
+        case "scissors":
+            if(computerChoice === "rock") {
+                alert("You lose, rock beats scissors.");
+            }
+            else if(computerChoice === "paper") {
+                alert("You win, scissors beats paper.");
+            }
+            else if(computerChoice === "scissors") {
+                alert("It's a tie, both chose scissors.");
+            }
+            else {
+                alert("ERROR");
+            }
+            break;
+    }
+}
+
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+playOneRound(playerChoice, computerChoice);
