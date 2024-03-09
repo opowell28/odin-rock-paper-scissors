33c69d0eb3298a79f7875fee61f3d8127a0ce677
Owen Powell <opowell2899@gmail.com>
2024-03-09 17:04:49 -0500

    Change getComputerChoice() to return result instead of assign to variable

--- scripts.js
+++ scripts.js
@@ -1,18 +1,16 @@
-let computerChoice;
-
 // randomly return either rock, paper, or scissors based on a random number
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 100) + 1;
 
     if(randNum >= 1 && randNum < 33) {
-        computerChoice = "rock";
+        return "rock";
     }
     else if(randNum >= 33 && randNum < 66) {
-        computerChoice = "paper";
+        return "paper";
     }
     else {
-        computerChoice = "scissors";
+        return "scissors";
     }
 }
 
-getComputerChoice();
+console.log(getComputerChoice());
